d5b2beedcfb682c237ae77bdcef3e8b22e08a49a
Jonas2134 <neo.stief.jon@gmail.com>
2024-11-10 20:22:11 +0100

    create game-info and dialog-add-player and finish the game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule  } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from '../game-info/game-info.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule, PlayerComponent],
+  imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule, GameInfoComponent],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
@@ -16,31 +21,37 @@ export class GameComponent {
   game: Game;
   currentCard: string | undefined = '';
 
-  constructor() {
+  constructor(public dialog: MatDialog) {
     this.game = new Game();
   }
 
   ngOnInit(): void {
-    this.newGame();
-  }
-
-  newGame() {
-    console.log(this.game);
+    
   }
 
   takeCard() {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game!.stack.pop();
       this.pickCardAnimation = true;
-      console.log('New card:' + this.currentCard);
-      console.log('Game is', this.game);
       
       setTimeout(() => {
         if (this.currentCard) {
           this.game.playerCards.push(this.currentCard);
         }
+        this.game.currentPlayer++;
+        this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
         this.pickCardAnimation = false;
       }, 1250);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name && name.length > 0) {
+        this.game.players.push(name);        
+      }
+    });
   }
 }
